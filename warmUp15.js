e583240929b3ce9490ec4c16f0bad81f27b7c418
Essam <esamfarg91@gmail.com>
2019-11-05 09:50:32 +0100

    the actual answer

--- warmUp15.js
+++ warmUp15.js
@@ -4,11 +4,37 @@
 //
 //
 // Note : you can`t use any built-in functions .
-function reverseStr (str) {
-	var arr = []
-	str = str.split(' ');
-	for (var i = str.length - 1; i >= 0; i--){
-		arr.push(str[i])
+// function reverseStr (str) {
+// 	var arr = []
+// 	str = str.split(' ');
+// 	for (var i = str.length - 1; i >= 0; i--){
+// 		arr.push(str[i])
+// 	}
+// 	return arr;
+// }
+
+function reverseStr (str) { 
+	
+	var arr = []; 
+	var cnt = 0;
+	var returnedStr = '';
+	var i = 0;
+
+	while(i < str.length){   //the while loop soes what str.split(' ')
+		if(str[i] !== ' ' && arr[cnt] === undefined){
+			arr[cnt] = str[i]
+			i++; 
+		} else if (str[i] !== ' ' && arr[cnt] !== undefined){
+      arr[cnt] += str[i]
+			i++;
+    } else {
+			cnt ++;
+			i++;
+		}
 	}
-	return arr;
+	
+	for (var i = arr.length - 1; i >= 0; i--){
+		returnedStr += arr[i] + ' ';	// instead of result.push(arr[i])
+	}
+	return returnedStr;
 }
